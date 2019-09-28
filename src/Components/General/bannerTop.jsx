d2fc70df2fed30651847c2a90a0dd91f2afcc6fc
Glenn Lozano Tapia <Glenn Lozano Tapia@DESKTOP-LTQH1FD>
2019-09-28 14:14:48 -0500

    TopBAR

--- src/Components/General/bannerTop.jsx
+++ src/Components/General/bannerTop.jsx
@@ -11,9 +11,38 @@ class BannerTop extends Component{
   render(){
     debugger;
     return (
-      <div id="banner" style={styles.banner}>
+      <div id="bannerTop" style={styles.banner}>
         <h1 className="navbar-header">Eventrinet<span className="badge badge-secondary"></span></h1>
-        <nav className="navbar navbar-expand-md fixed-top"></nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Eventos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Convocatoria</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Opciones
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="#">Mis Inscripciones</a>
+                  <a class="dropdown-item" href="#">Mis Propuestas</a>
+                  <a class="dropdown-item" href="#">Organizador</a>
+                  <a class="dropdown-item" href="#">Presidente</a>
+                  <a class="dropdown-item" href="#">Evaluador</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
