10e215944dcf41c87dd1b8d53d7daf9a02d2b6a2
Andres VA <a20141684@pucp.pe>
2019-10-01 16:33:34 -0500

    la fregada on

--- src/Components/General/bannerTop.jsx
+++ src/Components/General/bannerTop.jsx
@@ -50,7 +50,7 @@ class BannerTop extends Component{
                 </Modal>
             </section>
           </div>
-        </div    
+        </div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <button class="navbar-toggler" type="button" backgroundcolor='#002D3D' data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
