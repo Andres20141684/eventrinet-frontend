1e6a95fe47767ef9fa8fd5e6ed3243937888264b
sebas5750421 <a20143071@pucp.pe>
2019-10-23 11:14:10 -0500

    No he tocado nada de los demas :'v

---
+++ src/Pages/ProposerDetailProposal.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import BannerTop from '../Components/General/bannerTop';
+import BannerBottom from '../Components/General/bannerBottom'
+
+import '../styles/style_record.css'; 
+import 'react-tabs/style/react-tabs.css'
+import 'react-table/react-table.css'
+import './../styles/style_gig_tittle.css'
+import Row from 'react-bootstrap/Row';
+
+const Networking = require('../Network/Networking.js') ;
+
+function Botones(){
+    return ( 
+    <div>
+        <h2><br/></h2>
+        <h3>
+        <button class="mybutton" style={{float:'left'}}>Atras</button>
+        <button class="mybutton" style={{float:'right'}}>Guardar</button>
+        <br/><br/>
+        </h3>
+    </div>
+    )
+}
+function MainTittle(){
+    return ( 
+    <div style={{marginLeft:15}}>
+        <h1><br/>Community 01 - 15 Marzo</h1>
+        <br/><br/>
+    </div>    
+    )
+}
+
+
+class ProposerDetailProposal extends Component{
+    state = {
+        bannTop : BannerTop,
+        bannBot : BannerBottom,
+
+    }
+    
+   
+    render(){
+        return(
+            <div > 
+            <this.state.bannTop />
+            <div style={{justifyContent: "center", alignItems: "center"}}>
+            <MainTittle/>
+            
+            <div class="panel-group"  >
+            <div class="panel panel-default ">
+                <div class="panel-heading"><h1>Detalle de la propuesta</h1></div>
+                <div class="panel-body">
+                <Row >
+                <div class="form-group col-md-6">
+                    <label >Nombre</label>
+                    <input 
+                        style = {{backgroundColor:"#e9ecef", opacity:1,color: "#828282"}}
+                        type="text" 
+                        name='nombre'
+                        class="form-control" 
+                        id="id_name"
+                        value="{this.props.name}" readonly/>
+                </div>
+                </Row>
+                <Row>
+                <div class="form-group col-md-6">
+                    <label>Descripcion</label>
+                    <textarea 
+                        style = {{backgroundColor:"#e9ecef", opacity:1,color: "#828282"}}
+                        type="text" 
+                        name='descripcion'
+                        class="form-control" 
+                        id="id_description"
+                        value={this.props.description}  readonly/>
+                </div>
+                </Row>
+                <Row>
+                <div class="form-group col-md-6">
+                    <label >Estado</label>
+                    <input 
+                        style = {{backgroundColor:"#e9ecef", opacity:1,color: "#828282"}}
+                        type="text" 
+                        name='estado'
+                        class="form-control" 
+                        id="id_state"
+                        value={this.props.state} readonly/>
+                </div>
+                </Row>
+                <Row>
+                <div class="form-group col-md-6">
+                    <label >Fase Actual</label>
+                    <input 
+                        style = {{backgroundColor:"#e9ecef", opacity:1,color: "#828282"}}
+                        type="text" 
+                        name='fase'
+                        class="form-control" 
+                        id="id_phase"
+                        value={this.props.phase} readonly/>
+                </div>
+                </Row>
+                <Row>
+                <div class="form-group col-md-6">
+                    <label >Archivo</label>
+                    <input 
+                        style = {{backgroundColor:"#e9ecef", opacity:1,color: "#828282"}}
+                        type="text" 
+                        name='lugar'
+                        class="form-control" 
+                        id="id_place"
+                        placeholder='Lugar'
+                        value={this.props.lugar} readonly/>
+                </div>
+                </Row>
+                </div>
+            </div>
+            </div> 
+            <br/><br/>
+            <Botones/> 
+            <br/><br/>
+            </div>
+        <this.state.bannBot/>
+        </div>
+            
+        );
+    }
+}
+export default ProposerDetailProposal;
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import BannerLogin from '../Components/General/bannerLogin';
 import BannerBottom from '../Components/General/bannerBottom'
-import '../styles/style_record.css'; 
-import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBIcon, MDBBtn } from 'mdbreact';
+import '../styles/style_signUp.css'; 
+import {Link}  from "react-router-dom";
 
 
 var request=null;
@@ -18,123 +18,46 @@ class SingUp extends Component{
     }
     
     render(){
-
-        return(
-        <div>            
-            <body>  
-                <div class="component-header"  width="300">
-                <a class="component-logo customizable chart" href='/' title="Volver a pagina principal">
-                    <img class="component-logo" src="logo.png"  width="240"/> 
-                </a>
-                </div>                
-                <section class="container-fluid bg">
-                    <section class="row justify-content-center">
-                        <section class="col-12  col-sm-6 col-md-3">
-                          <br/><br/><br/><br/>
-                        <MDBContainer>
-                        <MDBRow>
-        <MDBCol md="6">
-          <MDBCard>
-            <MDBCardBody>
-              <form>
-                <p className="h4 text-center py-4">Sign up</p>
-                <div className="grey-text">
-                  <MDBInput
-                    label="Nombre"
-                    icon="user"
-                    group
-                    type="text"
-                    validate
-                    error="wrong"
-                    success="right"
-                  />
-                  <MDBInput
-                    label="Correo Electrónico"
-                    icon="envelope"
-                    group
-                    type="email"
-                    validate
-                    error="wrong"
-                    success="right"
-                  />
-                  <MDBInput
-                    label="Confirma tu correo electrónico"
-                    icon="exclamation-triangle"
-                    group
-                    type="text"
-                    validate
-                    error="wrong"
-                    success="right"
-                  />
-                  <MDBInput
-                    label="Contraseña"
-                    icon="lock"
-                    group
-                    type="password"
-                    validate
-                  />
+      return(
+        <div>
+          <div class="component-header"  width="300"  style={{paddingLeft:20}}>
+            <a class="component-logo customizable chart" href='/' title="Volver a pagina principal">
+                <img class="component-logo" src="logo.png"  width="240"/> 
+            </a>
+          </div>
+          <div class="page-content">
+            <div class="form-v5-content">
+              <form class="form-detail" action="#" method="post">
+                <h2>Registrarse</h2>
+                <div class="form-row">
+                  <label for="full-name">Nombre Completo</label>
+                  <input type="text" name="full-name" id="full-name" class="input-text" placeholder="Nombre completo" required/>
+                  <i class="fa fa-user"></i>
                 </div>
-                <div className="text-center py-4 mt-3">
-                  <MDBBtn color="cyan" type="submit">
-                    Registrar
-                  </MDBBtn>
-                  <MDBBtn>
-                  <div width="200" height="500" effect="fadeInUp">                                    
-                  <a href={this.state.pagPrev}>
-                  <div class="g-signin2" align="center" data-onsuccess="onSignIn" >
-                      
-                  </div>
-                  </a>
-                  </div>
-                  </MDBBtn>
+                <div class="form-row">
+                  <label for="your-email">Email</label>
+                  <input type="text" name="your-email" id="your-email" class="input-text" placeholder="Email" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}"/>
+                  <i class="fa fa-envelope"></i>
+                </div>
+                <div class="form-row">
+                  <label for="password">Contraseña</label>
+                  <input type="password" name="password" id="password" class="input-text" placeholder="Contraseña" required/>
+                  <i class="fa fa-lock"></i>
+                </div>
+                <div class="form-row-last">
+                  <input type="submit" name="Registrarse" class="register" value="Registrarse"/>
+                </div>
+                <div class="hint-text small" style={{textAlign:"center"}}>
+                  ¿Ya tienes una cuenta?
+                  <a href="/login"  class="text-success">Iniciar sesión</a>
                 </div>
               </form>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-      </MDBContainer>     
-
-
-
-                        </section>
-                    </section>
-                </section>
-            </body>
-            <this.state.bannBot />
-        </div>)
+              
+            </div>
+          </div>
+          
+        </div>
+      )
     }
 }
-
-
 export default SingUp;
-
-/*<div class="form-container">
-                        <form action="/organActiveEvents" >
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Usuario</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1"  placeholder="Ingresar usuario"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Contraseña</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña"/>
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-block">Iniciar sesion</button>
-                            
-                        </form>
-                        <div class="right-box">
-                            <br/>
-                            <label for="exampleInputEmail1"  style={{textAlignVertical: "center",textAlign: "center"}}>Ingresar con cuenta gmail</label>
-                            <section class="loginButton">
-                            <script src= "./login.js"></script>
-
-                                <div width="200" height="500" effect="fadeInUp">                                    
-                                    <a href={this.state.pagPrev}>
-                                    <div class="g-signin2" align="center" data-onsuccess="onSignIn" >
-                                        
-                                    </div>
-                                    </a>
-                                </div>
-                            </section>
-                        </div>
-                        </div>*/
