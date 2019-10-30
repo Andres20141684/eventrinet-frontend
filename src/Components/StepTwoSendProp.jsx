a736d3b4e4adfd3f07a8e7a20b9d18cc79b9e462
sebas5750421 <a20143071@pucp.pe>
2019-10-30 17:10:32 -0500

    c jala el curso

--- src/Components/StepTwoSendProp.jsx
+++ src/Components/StepTwoSendProp.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import 'react-simple-datepicker/dist/index.css';
-import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import "react-datepicker/dist/react-datepicker.css";
 import '../styles/style_sheets.css'
-import DatePicker from "react-datepicker"; 
 import "react-datepicker/dist/react-datepicker.css";
 import '../styles/style_sheets.css';
 
@@ -13,13 +12,13 @@ export default class StepTwoSendProp extends React.Component {
   
   render () {
     return (
-      <div>
-        <div class="panel-group">
+      <div >
+        <div class="panel-group mx-auto" style={{width: "600px"}}>
           <div class="panel panel-default">
             <div class="panel-heading"><h1>Descripcion</h1></div>
             <div class="panel-body">
             <Row >
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-12">
                 <label >Actividades</label>
                 <input 
                     type="text" 
@@ -33,7 +32,7 @@ export default class StepTwoSendProp extends React.Component {
             </div>
             </Row>
             <Row>
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-12">
                 <label>Resumen</label>
                 <textarea 
                     type="text" 
@@ -52,32 +51,31 @@ export default class StepTwoSendProp extends React.Component {
           <div class="panel panel-default">
             <div class="panel-heading"><h1>Categorías</h1></div>
             <div class="panel-body">
-              <div class="form-group col-md-6">
-                <div class="form-group">
-                  <Row>
-                    <select class="form-control" id="id_selectCategory">
-                      <option>Machine Learning</option>
-                      <option>Redes</option>
-                      <option>Inteligencia </option>
-                      <option>Automirision experta</option>
-                      <option>sebiwis</option>
-                    </select>
-                  </Row>
-                  <Row>
-                    <button type="button" class="btn btn-success">Agregar</button>
-                  </Row>
-                  
+            <Row >
+            <div class="form-group col-md-12">              
+                <div class="col-xs-9 col-md-9" style={{paddingLeft:0}}>
+                  <select class="form-control" id="id_selectCategory">
+                    <option>Machine Learning</option>
+                    <option>Redes</option>
+                    <option>Inteligencia </option>
+                    <option>Automirision experta</option>
+                    <option>sebiwis</option>
+                  </select>
+                </div>
+                <div class=" col-xs-3 col-md-3"style={{float: "left", paddingRight:0}}>
+                  <button type="button" class="btn btn-success" style={{width:"126px"}}>Agregar</button>
                 </div>
               </div>
+              </Row>
             </div>
           </div>
           <br></br>
           <div class="panel panel-default">
             <div class="panel-heading"><h1>Archivo</h1></div>
             <div class="panel-body">
-              <div class="form-group col-md-6">
+              <div class="form-group col-md-12"style={{paddingLeft:0}}>
               <label>Subir propuesta. El archivo debe estar en formato PDF (extension PDF)</label>
-              <div class="custom-file">
+              <div class="custom-file" >
                 <input type="file" class="custom-file-input" id="inputGroupFile01"
                   aria-describedby="inputGroupFileAddon01"/>
                 <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
