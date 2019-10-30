da21d65f1ed6917ff2dba57b0be901d5f413f95d
JeremiCS97 <jeremi.cardenas@pucp.pe>
2019-10-30 17:53:57 -0500

    TopBanner: Botones - Cambiamos para que aparezca la manito matona

--- src/Components/General/bannerTop.jsx
+++ src/Components/General/bannerTop.jsx
@@ -264,15 +264,15 @@ class BannerTop extends Component{
           <div className="collapse navbar-collapse" id="navbarNavDropdown" style={{}}>
             <ul className="nav navbar-nav">
               <li className="nav-item">
-                <a className="nav-link"  onClick={this.handleClickInicio}><b><font size="3" color="#6CDCD6">Inicio</font></b><span className="sr-only">(current)</span></a>
+                <Link className="nav-link"  onClick={this.handleClickInicio}><b><font size="3" color="#6CDCD6">Inicio</font></b><span className="sr-only">(current)</span></Link>
               </li>
               
               <li className="nav-item">
-                <a className="nav-link" onClick={this.handleClicEvents}><b><font size="3" color="#6CDCD6">Eventos</font></b></a>
+                <Link className="nav-link" onClick={this.handleClicEvents}><b><font size="3" color="#6CDCD6">Eventos</font></b></Link>
               </li>
               
               <li className="nav-item" >
-                <a className="nav-link" onClick={this.handleClickAnnoucements} ><b><font size="3" color="#6CDCD6">Convocatoria</font></b></a>
+                <Link className="nav-link" onClick={this.handleClickAnnoucements} ><b><font size="3" color="#6CDCD6">Convocatoria</font></b></Link>
               </li>
               <li className="nav-item" className="nav dropdown" id="nav-item-opciones">
                 <Link className="nav-link dropdown-toggle" to="#" data-toggle="dropdown" role="button"  aria-haspopup="true" aria-expanded="false"><b><font size="3" color="#6CDCD6">Opciones</font></b></Link>
