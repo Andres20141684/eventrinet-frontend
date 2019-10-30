03f3aedc82230bb8b3cbae02777b6295fe2c093a
sebas5750421 <a20143071@pucp.pe>
2019-10-30 09:51:38 -0500

    logOut desde pag principal

--- src/Components/General/bannerTop.jsx
+++ src/Components/General/bannerTop.jsx
@@ -4,6 +4,8 @@ import {Link}  from "react-router-dom";
 import Dashboard from '../Dashboard';
 import NewIni from './NewIni';
 import OrganActiveEvents from "./../../Pages/OrganActiveEvents.jsx";
+import GoogleLogout from 'react-google-login';    
+
 
 function initialState(){
   let linkLogin = document.getElementById("linkLogin")
@@ -71,6 +73,22 @@ function setRoles(listRoles){
 
 }
 
+
+function signUp(){
+  try{
+    const auth2 = window.gapi.auth2.getAuthInstance()
+    console.log("auth2 ", auth2)
+    if (auth2 != null) {
+      auth2.signOut().then(
+        auth2.disconnect().then(this.props.onLogoutSuccess)
+      )
+    }
+  }catch(err){
+    console.log(err)
+  }
+  
+}
+
 class BannerTop extends Component{
   
   constructor(props) {
@@ -89,6 +107,7 @@ class BannerTop extends Component{
     }
     this.handleNextChildComponentChange=this.handleNextChildComponentChange.bind(this);
     this.handleNextChildComponentChangeProps=this.handleNextChildComponentChangeProps.bind(this);
+    
 
   }  
   handleClickEventos(){
@@ -154,16 +173,11 @@ class BannerTop extends Component{
   }
 
   clickLogOut () {
-    try{
-      console.log("HAAAAAAAAAAAAAAAAAAAAAAA");
+    try{      
       let linkLogin = document.getElementById("linkLogin")
-      console.log("linkLogin 0");
       let linkSignUp = document.getElementById("linkSignUp")
-      console.log("linkSignUp 0");
       let myavatar = document.getElementById("myavatar")
-      console.log("myavatar 0");
       let itemOpciones = document.getElementById("nav-item-opciones")
-      console.log("itemOpciones 0");
       let nameUser = document.getElementById("nameUser")
 
       linkLogin.style.display = "block"
@@ -171,31 +185,49 @@ class BannerTop extends Component{
       myavatar.style.display = "none"
       itemOpciones.style.display = "none"
       nameUser.style.display = "none"
-      
-      sessionStorage.setItem("dataUser",null)
-      sessionStorage.setItem("currentPage",null)
-      
+
       let retrievedObject = sessionStorage.getItem('dataUser');
       let retrievedJson = JSON.parse(retrievedObject); 
       
       console.log("json borrado",retrievedJson)
+      
+      sessionStorage.setItem("dataUser",null)
+      sessionStorage.setItem("currentPage",null)
+
+      let tipoLogin = sessionStorage.getItem('tipoLogin');      
+      console.log("tipologin ",tipoLogin)
+      if (tipoLogin == "gmail"){
+        console.log("Iniciando logOut de gmail")
+        signUp()
+        console.log("Se concluyo logOut de gmail")
+      }
+
+      sessionStorage.setItem('tipoLogin',null)
+      
     }catch(err){
       console.log(err)
     }    
   }
-
   render(){
-    //debugger;
+    
     return (
-      <div id="bannerTop" style={styles.banner}><br/>
+      <div id="bannerTop" style={styles.banner}><br/>              
         <div className="list-inline-item d-flex flex-column flex-md-row align-items-center ">
           <div className="list-inline-item my-0 mr-md-auto font-weight-normal">
 
-          <Link onClick={this.handleClickInicio} target="_self" title="Volver al home">
-            <img src="piruleta_loquisima.png" className="img-fluid"  width="200"/></Link>
+          <a onClick={this.handleClickInicio} target="_self" title="Volver al home">
+            <img src="piruleta_loquisima.png" className="img-fluid"  width="200"/></a>
             
           </div>          
-          <div class="nav navbar-nav navbar-right ml-auto" style={{alignItems:"center",paddingRight:20}}>
+          <GoogleLogout
+                render={renderProps => (
+                  <a
+                    className="logout-button"
+                    onClick={renderProps.onClick}
+                  />                  
+                )}    
+            />
+          <div className="nav navbar-nav navbar-right ml-auto" style={{alignItems:"center",paddingRight:20}}>
               
               <div className="list-inline-item" align="right">
                 <a href="/signUp" id="linkSignUp" className="nav"  style={{color:"#6CDCD6",paddingRight:20}} >{this.state.SignUp}</a>
@@ -205,60 +237,60 @@ class BannerTop extends Component{
 
 
 
-              <li class="nav-item dropdown" id="myavatar"   >
+              <li className="nav-item dropdown" id="myavatar"   >
                 
-                  <Link to="#" data-toggle="dropdown" class="nav-link dropdown-toggle user-action">
-                    <img src="https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png" class="avatar" alt="Avatar"/>
+                  <Link to="#" data-toggle="dropdown" className="nav-link dropdown-toggle user-action">
+                    <img src="https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png" className="avatar" alt="Avatar"/>
                   </Link>
-                  <ul class="dropdown-menu dropdown-menu-right">
-                      <li><Link to="#" class="dropdown-item"><i class="fa fa-user-o"></i> Perfil</Link></li>
-                      <li><Link to="#" class="dropdown-item"><i class="fa fa-sliders"></i> Ajustes</Link></li>
-                      <li class="divider dropdown-divider"></li>
-                      <li><Link to="/" class="dropdown-item"onClick={this.clickLogOut}><i class="material-icons" >&#xE8AC;</i> Cerrar sesion</Link></li>
+                  <ul className="dropdown-menu dropdown-menu-right">
+                      <li><Link to="#" className="dropdown-item"><i className="fa fa-user-o"></i> Perfil</Link></li>
+                      <li><Link to="#" className="dropdown-item"><i className="fa fa-sliders"></i> Ajustes</Link></li>
+                      <li className="divider dropdown-divider"></li>
+                      <li><Link to="/" className="dropdown-item"onClick={this.clickLogOut}><i className="material-icons" >&#xE8AC;</i> Cerrar sesion</Link></li>
                   </ul>
               </li> 
             </div>
           
         </div>
-        <div style={{paddingRight:20, paddingLeft:20}}><hr  class="line-top"/></div>
+        <div style={{paddingRight:20, paddingLeft:20}}><hr  className="line-top"/></div>
     
         <div>  
         
-        <nav class="navbar navbar-default navbar-expand-xl navbar" style={styles.navbar}>
-          <button class="navbar-toggler scrollbar scrollbar-primary" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="navbar-toggler-icon fa fa-bars" style={styles.fa} aria-hidden="true"></i>
+        <nav className="navbar navbar-default navbar-expand-xl navbar" style={styles.navbar}>
+          <button className="navbar-toggler scrollbar scrollbar-primary" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <i className="navbar-toggler-icon fa fa-bars" style={styles.fa} aria-hidden="true"></i>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarNavDropdown" style={{}}>
-            <ul class="nav navbar-nav">
-              <li class="nav-item">
-                <Link class="nav-link"  onClick={this.handleClickInicio}><b><font size="3" color="#6CDCD6">Inicio</font></b><span class="sr-only">(current)</span></Link>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown" style={{}}>
+            <ul className="nav navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link"  onClick={this.handleClickInicio}><b><font size="3" color="#6CDCD6">Inicio</font></b><span className="sr-only">(current)</span></a>
               </li>
               
-              <li class="nav-item">
-                <Link class="nav-link" onClick={this.handleClicEvents}><b><font size="3" color="#6CDCD6">Eventos</font></b></Link>
+              <li className="nav-item">
+                <a className="nav-link" onClick={this.handleClicEvents}><b><font size="3" color="#6CDCD6">Eventos</font></b></a>
               </li>
               
-              <li class="nav-item" >
-                <Link class="nav-link" onClick={this.handleClickAnnoucements} ><b><font size="3" color="#6CDCD6">Convocatoria</font></b></Link>
+              <li className="nav-item" >
+                <a className="nav-link" onClick={this.handleClickAnnoucements} ><b><font size="3" color="#6CDCD6">Convocatoria</font></b></a>
               </li>
-              <li class="nav-item" class="nav dropdown" id="nav-item-opciones">
-                <Link class="nav-link dropdown-toggle" to="#" data-toggle="dropdown" role="button"  aria-haspopup="true" aria-expanded="false"><b><font size="3" color="#6CDCD6">Opciones</font></b></Link>
-                <ul class="dropdown-menu">
-                  <li><Link id="itemMisInscrip" class="nav-link" to="#"><b><font size="3">Mis inscripciones</font></b></Link></li>
-                  <li><Link id="itemMisProp" class="nav-link" to="/propoMyProposals"><b><font size="3">Mis propuestas</font></b></Link></li>
-                  <div class="dropdown-divider"></div>
-                  <li><Link id="itemOrga" class="nav-link" onClick={this.handleClicOrganizadorEventos}><b><font size="3">Organizador</font></b></Link></li>
-                  <li><Link id="itemPresi" class="nav-link" to="/presidentEvents"><b><font size="3">Presidente</font></b></Link></li>
-                  <li><Link id="itemEval" class="nav-link" to="/EvaluadorEventos" ><b><font size="3 ">Evaluador</font></b></Link></li>
+              <li className="nav-item" className="nav dropdown" id="nav-item-opciones">
+                <Link className="nav-link dropdown-toggle" to="#" data-toggle="dropdown" role="button"  aria-haspopup="true" aria-expanded="false"><b><font size="3" color="#6CDCD6">Opciones</font></b></Link>
+                <ul className="dropdown-menu">
+                  <li><Link id="itemMisInscrip" className="nav-link" to="#"><b><font size="3">Mis inscripciones</font></b></Link></li>
+                  <li><Link id="itemMisProp" className="nav-link" to="/propoMyProposals"><b><font size="3">Mis propuestas</font></b></Link></li>
+                  <div className="dropdown-divider"></div>
+                  <li><a id="itemOrga" className="nav-link" onClick={this.handleClicOrganizadorEventos}><b><font size="3">Organizador</font></b></a></li>
+                  <li><Link id="itemPresi" className="nav-link" to="/presidentEvents"><b><font size="3">Presidente</font></b></Link></li>
+                  <li><Link id="itemEval" className="nav-link" to="/EvaluadorEventos" ><b><font size="3 ">Evaluador</font></b></Link></li>
                 </ul>
               </li>
             </ul>
             
-            <form class="navbar-form form-inline">
-              <div class="input-group search-box" style={{alignItems:"center"}}>								
-                    <input type="text" id="search" class="form-control" placeholder="Buscar" style={{alignItems:"center"}}/>
-                    <span class="input-group-addon"><i class="material-icons">&#xE8B6;</i></span>
+            <form className="navbar-form form-inline">
+              <div className="input-group search-box" style={{alignItems:"center"}}>								
+                    <input type="text" id="search" className="form-control" placeholder="Buscar" style={{alignItems:"center"}}/>
+                    <span className="input-group-addon"><i className="material-icons">&#xE8B6;</i></span>
               </div>
             </form>            
           </div>
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -31,6 +31,7 @@ class Login extends Component{
                     console.log("estamos accediendoo bbecita prrr");
                     console.log(connectedUser);
                     sessionStorage.setItem('dataUser', JSON.stringify(connectedUser));
+                    sessionStorage.setItem('tipoLogin',"usuario")
                     this.setState({redirect:true});
                     alert("Contraseña y/o usuario correcta!");
                   }else{
@@ -77,56 +78,56 @@ class Login extends Component{
     render(){
       return(
         <div>{this.renderRedirect()} 
-          <div class="component-header"  width="300"  style={{paddingLeft:20}}>
-            <a class="component-logo customizable chart" href='/' title="Volver a pagina principal">
-                <img class="component-logo" src="logo.png"  width="240"/> 
+          <div className="component-header"  width="300"  style={{paddingLeft:20}}>
+            <a className="component-logo customizable chart" href='/' title="Volver a pagina principal">
+                <img className="component-logo" src="logo.png"  width="240"/> 
             </a>
           </div>
-          <div class="page-content">
+          <div className="page-content">
 
-            <div class="form-v5-content">
-              <form class="form-detail"  type="post" onSubmit={this.onSubmitForm}>
+            <div className="form-v5-content">
+              <form className="form-detail"  type="post" onSubmit={this.onSubmitForm}>
                 <h2>Eventrinet</h2>
                 
-                <div class="form-row">
+                <div className="form-row">
                   <label for="your-user">Usuario</label>
-                  <input type="text" name="your-user" id="your-user" class="input-text" onChange={this.onChageInputName} onKeyDown={this.onKeyDownName} placeholder="Ingresar usuario" />
-                  <i class="fa fa-envelope"></i>
+                  <input type="text" name="your-user" id="your-user" className="input-text" onChange={this.onChageInputName} onKeyDown={this.onKeyDownName} placeholder="Ingresar usuario" />
+                  <i className="fa fa-envelope"></i>
                 </div>
-                <div class="form-row">
+                <div className="form-row">
                   <label for="password">Contraseña</label>
-                  <input type="password" name="password" id="password" class="input-text"  onChange={this.onChageInputPass} placeholder="Contraseña" required/>
-                  <i class="fa fa-lock"></i>
+                  <input type="password" name="password" id="password" className="input-text"  onChange={this.onChageInputPass} placeholder="Contraseña" required/>
+                  <i className="fa fa-lock"></i>
                 </div>
-                <div class="form-row-last">                  
-                  <input type="submit" name="Iniciar sesion"class="btn btn-primary btn-block" value="Iniciar Sesion"/>
+                <div className="form-row-last">                  
+                  <input type="submit" name="Iniciar sesion"className="btn btn-primary btn-block" value="Iniciar Sesion"/>
                 </div>
                                 
               </form>
               
-              <div id="errorMsg" class="alert alert-warning" role="alert" style={{marginBottom:0, marginTop:"20px", display:"none"}}></div>
+              <div id="errorMsg" className="alert alert-warning" role="alert" style={{marginBottom:0, marginTop:"20px", display:"none"}}></div>
               
-              <div class="col-xs-12">
-                  <div class="divider dividerSign">
-                  <strong class="divider-title ng-binding">o</strong>
+              <div className="col-xs-12">
+                  <div className="divider dividerSign">
+                  <strong className="divider-title ng-binding">o</strong>
                   </div>
                 </div>
               
                 <div >
-                  <section class="loginButton">
+                  <section className="loginButton">
                   <script src= "./login.js"></script>
                   <div  effect="fadeInUp">                                    
                       <a href={this.state.pagPrev}>
-                      <div class="g-signin2" align="center" data-onsuccess="onSignIn" />
+                      <div className="g-signin2" align="center" data-onsuccess="onSignIn" />
                       </a>
                   </div>
                   </section>
                 </div>
                 <br/>
 
-                <div class="hint-text small" style={{textAlign:"center"}}>
+                <div className="hint-text small" style={{textAlign:"center"}}>
                   ¿No tienes una cuenta?
-                  <Link to="/signUp"  class="text-success">Registrate ahora!</Link>
+                  <Link to="/signUp"  className="text-success">Registrate ahora!</Link>
                 </div>
                 <br/>
             </div>
