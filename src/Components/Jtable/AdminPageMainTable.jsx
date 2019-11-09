7d8ad793eee7feb7edf820d2a6480c6786f48790
sebas5750421 <a20143071@pucp.pe>
2019-11-08 21:23:00 -0500

    asdasd

--- src/Components/Jtable/AdminPageMainTable.jsx
+++ src/Components/Jtable/AdminPageMainTable.jsx
@@ -45,8 +45,14 @@ class AdminPageMainTable extends Component {
    handleClick = () => {
     console.log('this is:', this);
   }
-  handleClickPermiso = () => {
+  handleClickPermisos (idUsuario,correo,nombComp) {
     this.handleNextChildComponentChange(FormOtorgarPermisos);
+    this.handleNextChildComponentChangeProps(
+        {idUser:idUsuario,
+          email:correo,
+          nombre: nombComp
+        }
+    );
 
   }
    renderTableData() {
@@ -56,18 +62,13 @@ class AdminPageMainTable extends Component {
             <tr >
                 <td>{nombComp}</td>
                 <td>{correo}</td>
-                <td>{fechaIniPermiso}</td>
+                <td>{fechaIniPermiso}</td>                
                 <td>{fechaFinPermiso}</td>
-                <td>
-                    
-                    <ActionButton
-                        idUser_recived={this.state.idUser_recived} 
-                        button_class ="fa fa-times" 
-                        onNextChildComponentChange={this.props.onNextChildComponentChange}
-                        onNextChildComponentChangeProps={this.props.onNextChildComponentChangeProps}
-                        redirect_to="/"
-                  />
-               </td> 
+                <td><p data-placement="top" data-toggle="tooltip" title="Editar permisos" >
+                    <button class="btn btn-primary btn-xs" 
+                            data-title="Edit" data-toggle="modal" data-target="#edit" 
+                            onClick={this.handleClickPermiso(idUsuario,correo,nombComp)} ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+                <td><p data-placement="top" data-toggle="tooltip" title="Borrar"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td> 
             </tr>
         )
         })
@@ -75,11 +76,12 @@ class AdminPageMainTable extends Component {
     renderTableHeader() {
       return (
          <tr>
-             <th width="30%">Nombre completo</th>
-             <th width="30%">Correo</th>
-             <th width="15%">Vigencia Inicio</th>
-             <th width="15%">Vigencia Fin</th>
-             <th width="10%">Eliminar</th>
+             <th width="35%">Nombre completo</th>
+             <th width="27%">Correo</th>
+             <th width="12%">Vigencia Inicio</th>
+             <th width="12%">Vigencia Fin</th>
+             <th width="7%">Editar</th>
+             <th width="7%">Eliminar</th>
          </tr>
 
      )
@@ -97,9 +99,11 @@ class AdminPageMainTable extends Component {
             <div class="panel panel mypanel ">
                 <div class="panel-heading" style={{backgroundColor:"#ffff", color:"#333"}}>
                     <h3>Lista de organizadores que pueden crear evento</h3>
-                    <a  class="pull-right" onClick={this.handleClickPermiso} 
-                        style={{marginRight:30,marginBottom:20}}>Otorgar permiso</a>
+                    
                 </div>
+                <br/>
+                <input class="form-control" id="myInput" type="text" placeholder="Buscar.."/>
+                <br/>
                 <div  class="table-responsive">
                     <table class="table  table-hover" >
                     <thead  style={{backgroundColor:"#002D3D", color:"#6CDCD6"}}>
